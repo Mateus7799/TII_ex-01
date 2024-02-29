7cda86e230b4b7460dfe0517956efef8b57c4d17
peuaz <peuaz@DESKTOP-HE4L3U6>
2024-02-29 11:06:45 -0300

    Exercício 01

---
+++ SomadeInteiros/src/Soma/Soma.java
@@ -0,0 +1,10 @@
+package Soma;
+
+public class Soma {
+    public static void main(String[] args) {
+        int num1 = 9;
+        int num2 = 6;
+        int soma = num1 + num2;
+        System.out.println("A soma de " + num1 + " e " + num2 + " é " + soma);
+    }
+}
